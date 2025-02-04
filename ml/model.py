7acd4f0bbd4739170756ce58d645b86b5443a816
E.Trev <116616604+etrev17@users.noreply.github.com>
2025-02-04 06:39:09 -0800

    Update model.py
    add libraries and apply function

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+import pandas as pd
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +21,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(min_samples_split=30)
+    model.fit(X_train,y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -119,10 +122,8 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data, categorical_features, label, training=False, encoder=encoder, lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
