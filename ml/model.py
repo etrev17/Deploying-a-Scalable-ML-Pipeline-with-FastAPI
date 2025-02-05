947df119d40de834c7a8837b44cf2881c5a3ee5c
E.Trev <116616604+etrev17@users.noreply.github.com>
2025-02-04 18:46:41 -0800

    Update model.py
    add libraries

--- ml/model.py
+++ ml/model.py
@@ -3,6 +3,7 @@ from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
